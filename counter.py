ecda144cb135b689e3d50d15c7e9e1e767a13ebe
t-bence <toth.bence.mihaly@gmail.com>
2024-11-17 20:33:35 +0100

    Fix MIME type

--- counter.py
+++ counter.py
@@ -7,8 +7,9 @@ def increment_count(count: int) -> int:
 def get_response(count: int = None) -> func.HttpResponse:
     value = count if count else -1
     status_code = 200 if count else 400
-    
+
     return func.HttpResponse(
         json.dumps(dict(visitors=value)),
-        status_code=status_code
+        status_code=status_code,
+        mimetype="application/json"
     )
