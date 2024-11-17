80c7f293cf8aa3fb1991987389f9be0d13c72e72
t-bence <toth.bence.mihaly@gmail.com>
2024-11-17 21:30:44 +0100

    Blob input

--- function_app.py
+++ function_app.py
@@ -4,11 +4,19 @@ from counter import increment_count, get_response
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-@app.route(route="count_visitors")
-def count_visitors(req: func.HttpRequest) -> func.HttpResponse: # , input: str, output: func.Out[str]
-    logging.info('Python HTTP trigger function processed a request.')
+"""
+@app.blob_output(arg_name="output",
+                path="visitors/count.txt",
+                connection="CounterStorage")
+                """
 
-    input = "123"
+@app.route(route="count_visitors")
+@app.blob_input(arg_name="input",
+                path="visitors/count.txt",
+                connection="CounterStorage")
+
+def count_visitors(req: func.HttpRequest, input: str) -> func.HttpResponse: #output: func.Out[str]
+    logging.info('Python HTTP trigger function processed a request.')
 
     try:
         count = int(input)
