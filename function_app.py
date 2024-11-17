5dda2c25a215bf3dab03e7cc6310cc7e6e55b632
t-bence <toth.bence.mihaly@gmail.com>
2024-11-17 20:25:01 +0100

    Testing

--- function_app.py
+++ function_app.py
@@ -5,15 +5,17 @@ from counter import increment_count, get_response
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
 @app.route(route="count_visitors")
-def count_visitors(req: func.HttpRequest, input: str, output: func.Out[str]) -> func.HttpResponse:
+def count_visitors(req: func.HttpRequest) -> func.HttpResponse: # , input: str, output: func.Out[str]
     logging.info('Python HTTP trigger function processed a request.')
+
+    input = "123"
 
     try:
         count = int(input)
         logging.info(f"Read current count: {count}")
         new_count = increment_count(count)
         logging.info(f"Incremented count: {new_count}")
-        output.set(str(new_count))
+        #output.set(str(new_count))
         logging.info("Wrote new count to file")
     except Exception as e:
         logging.error(str(e))
